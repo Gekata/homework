4430bbcab24e99225060327350e4b1645beddc38
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-11-29 16:41:20 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte a = 5;
+        byte aa = 6;
+        short b = 22;
+        short bb = 14;
+        long c = 33;
+        long cc = 2;
+        double d = 1.5;
+        double dd = 3.3;
+        float e = 1.3f;
+        float ee = 2.4f;
+        int r = 12;
+        int rr = 92;
+
+        int g = r + rr;
+        long f = c / cc;
+        double x = d * dd;
+        float z = e - ee;
+        short v = (short) (b - bb);
+        byte m = (byte) (aa - a);
+        System.out.println(g);
+        System.out.println(f);
+        System.out.println(x);
+        System.out.println(z);
+        System.out.println(v);
+        System.out.println(m);
+
+
+    }
+}
