ec86252b18f6242292d00fef141bb1015b3f26f3
Олег Чабан <42222997+Gekata@users.noreply.github.com>
2019-11-29 17:56:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        byte a = 5; //задання значень
+        byte a = 5; //value
         byte aa = 6;
         short b = 22;
         short bb = 14;
@@ -16,20 +15,17 @@ public class Main {
         float ee = 2.4f;
         int r = 12;
         int rr = 92;
-
-        int g = r + rr; //арифметична дія
+        int g = r + rr; //arithmetic  equation
         long f = c / cc;
         double x = d * dd;
         float z = e - ee;
         short v = (short) (b - bb);
         byte m = (byte) (aa - a);
-        System.out.println(g); //вивід відповіді в консоль
+        System.out.println(g); //text in console
         System.out.println(f);
         System.out.println(x);
         System.out.println(z);
         System.out.println(v);
         System.out.println(m);
-
-
     }
 }
